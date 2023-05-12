8491ef0055bb2988a35ac6d8fe4c260bbedad761
Tommy Hak <thak@albany.edu>
2023-05-12 05:57:15 -0400

    started working on items on the home page, added placed holder for about me, and career objectives. Also added icons for work and contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import EmailIcon from '@material-ui/icons/Email';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className= "home">
+      <div className= "about">
+        <h2> Hi, My Name is Tommy </h2>
+        <div className= "career objective prompt"> <p>Enthusiastic and passionate individual working towards 
+          a completed Bachelors of Science degree from the State University at Albany. Aiming to use my skills 
+          and knowledge on Coding, Programming, IT, and Web design to satisfy the role in your company.  </p> 
+          <LinkedInIcon />
+          <EmailIcon />
+          <GitHubIcon />
+          </div>
+      </div>
+      <div className= "skills"></div>
+    </div>
   )
 }
 
