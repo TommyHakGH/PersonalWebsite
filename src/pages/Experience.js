cda9038506da092246e3dea35781c51af9cb02c6
Tommy Hak <thak@albany.edu>
2023-05-12 19:35:02 -0400

    added school, and icon for school from @materia-ui library

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
 
 function Experience() {
   return (
     <div className='experience'>
-      <VerticalTimeline></VerticalTimeline>
+      <VerticalTimeline lineColor= "#3e497a">
+        <VerticalTimelineElement 
+        className= "vertical-timeline-element--education" 
+        date="2012 - 2016"
+        iconStyle={{background: "3e497a", color: "#fff"}}
+        icon = {<SchoolIcon />}>
+
+        </VerticalTimelineElement>
+      </VerticalTimeline>
       </div>
   )
 }
