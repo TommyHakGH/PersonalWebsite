3714324f0404bac55e6f0d0eaed952695c139f68
Tommy Hak <thak@albany.edu>
2023-05-13 00:21:58 -0400

    added css styling for display page of project, still working on projectdisplaypage undefined error, project function is undefined for some reason

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,8 +1,15 @@
-import Proj1 from "../assets/Proj1.png"
+import Proj1 from "../assets/Proj1.png";
+import Proj2 from "../assets/Proj2.png";
 
     export const ProjectList = [
         {
-            name: "CSGO website",
-            image: Proj1
-        }
-    ]
+            name: "CSGO Website",
+            image: Proj1,
+            skills: "JavaScript, HTML, CSS",
+        },
+        {
+            name: "Skateboarding Website",
+            image: Proj2,
+            skills: "JavaScript, HTML, CSS",
+        },
+    ];
