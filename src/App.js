26ebbaa4f0ce9b5b437f32331e96dbedfd9acd8b
Tommy Hak <thak@albany.edu>
2023-05-13 00:56:54 -0400

    fixed earlier undefined function

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/project/:id" element={<ProjectDisplay />} />
+        <Route path="/project/id" element={<ProjectDisplay />} />
         <Route path="/experience" element={<Experience />} />
       </Routes>
       <Footer />
