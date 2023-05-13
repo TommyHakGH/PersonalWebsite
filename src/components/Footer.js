e8a7ef12ee68bc24bbc908316855670637ba717a
Tommy Hak <thak@albany.edu>
2023-05-13 07:26:06 -0400

    added links to the footer, as well as mouse interaction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,11 +10,11 @@ function Footer() {
   return (
     <div className="footer">
         <div className="SocialMedia/Contact">
-        <InstagramIcon />
-        <LinkedInIcon  />
-        <GitHubIcon />
-        <EmailIcon />
-        <ContactPhoneIcon />
+        <InstagramIcon cursor= 'pointer' onClick= {event => window.location.href='https://www.instagram.com/tommy_hak/'}/>
+        <LinkedInIcon cursor= 'pointer' onClick= {event => window.location.href='https://www.linkedin.com/in/tommy-hak-716619253/'}/>
+        <GitHubIcon cursor= 'pointer' onClick= {event => window.location.href='https://github.com/TommyHakGH?tab=repositories'}/>
+        <EmailIcon cursor= 'pointer' onClick= {event => window.location.href='https://mail.google.com/mail/?view=cm&fs=1&to=tommys.hak@gmail.com'}/>
+        <ContactPhoneIcon /> <p>+1(347)435-9209</p>
     </div>
     <p> &copy; 2023 TommyHak.com </p>
     </div>
